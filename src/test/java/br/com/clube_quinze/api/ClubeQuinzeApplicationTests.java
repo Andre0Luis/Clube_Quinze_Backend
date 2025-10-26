c1a37316a33148ffd41c37bd52cf7817ede99ae5
Andre0Luis <aluis283@gmail.com>
2025-10-26 15:24:39 -0300

    f1rst commit

---
+++ src/test/java/br/com/clube_quinze/api/ClubeQuinzeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clube_quinze.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubeQuinzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
